107d3b15666623698a97313adbf349bba766803e
Luiz Augusto <luiz158@gmail.com>
2015-01-17 12:06:55 -0200

    exemplo de atualizar....

---
+++ atualizar.sh
@@ -0,0 +1,9 @@
+cd /tmp
+git clone git@github.com:luiz158/agoncal-training-javaee6
+cd agoncal-training-javaee6
+git pull https://github.com/agoncal/agoncal-training-javaee6
+git status
+git push
+git status
+
+
